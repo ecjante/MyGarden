de190c537f706c8ee4b519d12cece1dd26084097
Enrico Jante <enrico.jante@gmail.com>
2018-02-20 17:02:01 -0800

    Complete Exercise 4: Update widget service to water only the plant that needs to be watered

--- app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
+++ app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
@@ -5,6 +5,7 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.view.View;
 import android.widget.RemoteViews;
 
 import com.example.android.mygarden.ui.MainActivity;
@@ -15,35 +16,42 @@ import com.example.android.mygarden.ui.MainActivity;
 
 public class PlantWidgetProvider extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int imageResource, int appWidgetId) {
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int imageResource,
+                                long plantId, boolean showWater, int appWidgetId) {
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.plant_widget);
+
+        views.setImageViewResource(R.id.widget_plant_image, imageResource);
+        views.setTextViewText(R.id.widget_plant_name, String.valueOf(plantId));
+
+        if (showWater) {
+            views.setViewVisibility(R.id.widget_water_button, View.VISIBLE);
+        } else {
+            views.setViewVisibility(R.id.widget_water_button, View.INVISIBLE);
+        }
+
         Intent intent = new Intent(context, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.plant_widget);
         views.setOnClickPendingIntent(R.id.widget_plant_image, pendingIntent);
 
         Intent wateringIntent = new Intent(context, PlantWateringService.class);
-        wateringIntent.setAction(PlantWateringService.ACTION_WATER_PLANTS);
-
+        wateringIntent.setAction(PlantWateringService.ACTION_WATER_PLANT);
+        wateringIntent.putExtra(PlantWateringService.EXTRA_PLANT_ID, plantId);
         PendingIntent wateringPendingIntent = PendingIntent.getService(context, 0,
                 wateringIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         views.setOnClickPendingIntent(R.id.widget_water_button, wateringPendingIntent);
-
-        views.setImageViewResource(R.id.widget_plant_image, imageResource);
 
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        PlantWateringService.startActionWaterPlants(context);
+        PlantWateringService.startActionUpdatePlantWidgets(context);
     }
 
     public static void updatePlantWidgets(Context context, AppWidgetManager appWidgetManager,
-                                          int imgRes, int[] appWidgetIds) {
+                                          int imgRes, long plantId, boolean showWater, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, imgRes, appWidgetId);
+            updateAppWidget(context, appWidgetManager, imgRes, plantId, showWater, appWidgetId);
         }
     }
 
